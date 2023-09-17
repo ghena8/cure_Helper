728c1e70b7175513b9bcfc062c9e06af58df5599
Ahmed Rowaihi <ahmedrowaihi1@gmail.com>
2023-09-18 02:50:45 +0300

    🐛 FIX(buttons): remove cutting paddings

--- lib/component/my_button.dart
+++ lib/component/my_button.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
+
 class button extends StatelessWidget {
   final Function()? onTab;
-  final String  text;
-  const button({super.key,required this.onTab,  required this.text});
+  final String text;
+  const button({super.key, required this.onTab, required this.text});
 
   @override
   Widget build(BuildContext context) {
-
     return GestureDetector(
-      onTap:onTab ,
-      child:Container(
+      onTap: onTab,
+      child: Container(
         height: 50,
-     padding:const EdgeInsets.all(25.0),
-     margin: const EdgeInsets.symmetric(horizontal: 25),
-      decoration: BoxDecoration( color: Colors.indigo.shade800,
-
-      borderRadius: BorderRadius.circular(30),
-    ),
-      child:  Center(
-          // Navigate to the second screen using a named route.
-        child: Text(text, style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.white,),),
-
+        // padding: const EdgeInsets.all(25.0), // this padding is causing the problem
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.indigo.shade800,
+          borderRadius: BorderRadius.circular(30),
         ),
-
+        child: Center(
+          // Navigate to the second screen using a named route.
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/component/start_button.dart
+++ lib/component/start_button.dart
@@ -1,29 +1,33 @@
 import 'package:flutter/material.dart';
+
 class start_button extends StatelessWidget {
   final Function()? onTab;
-  final String  text;
-  const start_button({super.key,required this.onTab,  required this.text});
+  final String text;
+  const start_button({super.key, required this.onTab, required this.text});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap:onTab ,
-      child:Container(
-        height:50,
-       width: 200,
-        padding:const EdgeInsets.all(40.0),
-
-        decoration: BoxDecoration( color: Colors.white,
-
+      onTap: onTab,
+      child: Container(
+        height: 50,
+        width: 200,
+        // padding: const EdgeInsets.all(40.0), // this padding is causing the problem
+        decoration: BoxDecoration(
+          color: Colors.white,
           borderRadius: BorderRadius.circular(30),
         ),
-        child:  Center(
+        child: Center(
           // Navigate to the second screen using a named route.
           child: Text(
-            text, style:  TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: Colors.black,),),
-
+            text,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Colors.black,
+            ),
+          ),
         ),
-
       ),
     );
   }
